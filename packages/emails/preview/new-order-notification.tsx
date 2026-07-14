108c303be53ad974513b58d4d7dda3d6ee56e512
Maciej Godek <starkit.rental@gmail.com>
2026-07-14 23:08:21 +0200

    feat(emails): dodać podgląd szablonów

---
+++ packages/emails/preview/new-order-notification.tsx
@@ -0,0 +1,19 @@
+import {
+  NewOrderNotification,
+  type NewOrderNotificationProps,
+} from "../src/index";
+
+function NewOrderNotificationPreview(props: NewOrderNotificationProps) {
+  return <NewOrderNotification {...props} />;
+}
+
+NewOrderNotificationPreview.PreviewProps = {
+  customerName: "Alicja Nowak",
+  orderNumber: "ZAM-2026-001",
+  orderUrl: "https://app.example.test/orders/order-preview",
+  rentalEndDate: "23.07.2026",
+  rentalStartDate: "20.07.2026",
+  totalAmount: "1 299,00 zł",
+} satisfies NewOrderNotificationProps;
+
+export default NewOrderNotificationPreview;
